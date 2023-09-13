e6f6b5f9e48b919bc388cccdf2cf3baeff023522
walkerjian <walkerjian@gmail.com>
2023-09-13 10:58:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,75 +1,33 @@
-// create web server
-// 1. load express module
-const express = require('express')
-// 2. create web server
-const app = express()
-// 3. set port
-const port = 3000
-// 4. handle request and response
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// 5. start server
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
-})
-// Path: comments.js
-// create web server
-// 1. load express module
-const express = require('express')
-// 2. create web server
-const app = express()
-// 3. set port
-const port = 3000
-// 4. handle request and response
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// 5. start server
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
-})
-// Path: comments.js
-// create web server
-// 1. load express module
-const express = require('express')
-// 2. create web server
-const app = express()
-// 3. set port
-const port = 3000
-// 4. handle request and response
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// 5. start server
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
-})
-// Path: comments.js
-// create web server
-// 1. load express module
-const express = require('express')
-// 2. create web server
-const app = express()
-// 3. set port
-const port = 3000
-// 4. handle request and response
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// 5. start server
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
-})
-// Path: comments.js
-// create web server
-// 1. load express module
-const express = require('express')
-// 2. create web server
-const app = express()
-// 3. set port
-const port = 3000
-// 4. handle request and response
-app.get('/', (req, res) => {
+// create web server using Express
+const express = require('express');
+const app = express();
+const path = require('path');
 
+// load data
+const { comments } = require('./data');
 
+// set up static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// set up comments route
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// set up comments/:id route
+app.get('/comments/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const comment = comments.find((comment) => comment.id === id);
+  res.json(comment);
+});
+
+// start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
