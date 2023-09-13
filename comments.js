339604cc86fb11af8c4e411b0cccdc0d3b950b72
walkerjian <walkerjian@gmail.com>
2023-09-13 10:51:39 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,75 @@
+// create web server
+// 1. load express module
+const express = require('express')
+// 2. create web server
+const app = express()
+// 3. set port
+const port = 3000
+// 4. handle request and response
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// 5. start server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+// Path: comments.js
+// create web server
+// 1. load express module
+const express = require('express')
+// 2. create web server
+const app = express()
+// 3. set port
+const port = 3000
+// 4. handle request and response
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// 5. start server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+// Path: comments.js
+// create web server
+// 1. load express module
+const express = require('express')
+// 2. create web server
+const app = express()
+// 3. set port
+const port = 3000
+// 4. handle request and response
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// 5. start server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+// Path: comments.js
+// create web server
+// 1. load express module
+const express = require('express')
+// 2. create web server
+const app = express()
+// 3. set port
+const port = 3000
+// 4. handle request and response
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// 5. start server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+// Path: comments.js
+// create web server
+// 1. load express module
+const express = require('express')
+// 2. create web server
+const app = express()
+// 3. set port
+const port = 3000
+// 4. handle request and response
+app.get('/', (req, res) => {
+
+
